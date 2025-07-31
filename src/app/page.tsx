6b50c0d483a5cc8d4ca3ac97bff4d9315dbccd82
LACASAFITOUT D <lacasafitout@gmail.com>
2025-07-31 12:51:43 +0000

    remove signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/signup');
+    router.replace('/dashboard');
   }, [router]);
 
   return null;
