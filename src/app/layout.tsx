ac96f820f2e03234716b9de3e2680fbc9ca4f414
LACASAFITOUT D <lacasafitout@gmail.com>
2025-08-01 18:25:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,7 +118,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning>
+      <body className="font-body antialiased">
         <ModulesProvider>
           <DashboardLayout>
             {children}
